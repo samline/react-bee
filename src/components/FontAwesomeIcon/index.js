4d88e1943dfd9285aacf3504a4797183e9375e6f
Samuel Olvera <jsamline@gmail.com>
2023-05-17 04:40:03 -0600

    update

--- src/components/FontAwesomeIcon/index.js
+++ src/components/FontAwesomeIcon/index.js
@@ -8,17 +8,23 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { Icon } from './styles'
 
 /* Libraries */
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from './fonts/free-brands-svg-icons'
+import { fad } from './fonts/pro-duotone-svg-icons'
+import { fal } from './fonts/pro-light-svg-icons'
+import { far } from './fonts/pro-regular-svg-icons'
+import { fas } from './fonts/pro-solid-svg-icons'
+import { fat } from './fonts/pro-thin-svg-icons'
+import { fasl } from './fonts/sharp-light-svg-icons'
+import { fasr } from './fonts/sharp-regular-svg-icons'
+import { fass } from './fonts/sharp-solid-svg-icons'
 
 /* Settings */
-library.add(fab, far, fas)
+library.add(fab, fad, fal, far, fas, fasl, fasr, fass, fat)
 
 /**
  * FontAwesomeIcon
  * @param {string} icon (required) - The icon to use. (e.g. 'chevron-right')
- * @param {string} lib - The library to use. - (e.g. 'fas', 'fab', 'far') - default: 'fas'
+ * @param {string} lib - The library to use. - (e.g. fab, fad, fal, far, fas, fasl, fasr, fass, fat) - default: 'fas'
  * @param {reference} innerRef - you can still assign to ref
  * @param {...any} props
  * @returns component
